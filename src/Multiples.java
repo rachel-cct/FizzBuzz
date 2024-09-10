e234752e556a08d38c6c5672e67a2d49720b63c9
rachelcheng <rachelct.cheng@mail.utoronto.ca>
2024-09-09 23:33:40 -0400

    im literally gonna kms why6 are there so mnay errors

--- src/Multiples.java
+++ src/Multiples.java
@@ -3,12 +3,17 @@ class Multiples {
     public static void multiples() {
         int count = 0;
         for (int y = 1; y < 1000; y++); {
-            boolean div3 = y % 3 == 0;
-            boolean div5 = y % 5 == 0;
-            if (div3 || div5) {
-                count++;
-            }
+            count = getCount(count);
         }
         System.out.println(count);
+    }
+
+    private static int getCount(int count) {
+        boolean div3 = y % 3 == 0;
+        boolean div5 = y % 5 == 0;
+        if (div3 || div5) {
+            count++;
+        }
+        return count;
     }
 }
