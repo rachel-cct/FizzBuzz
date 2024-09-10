274aa3a4d2b4c5eeb99f88cc4655383d0837e227
rachelcheng <rachelct.cheng@mail.utoronto.ca>
2024-09-10 00:11:13 -0400

    RIRI HELPED ME FINISH REDUCEE

--- src/REDUCEE.java
+++ src/REDUCEE.java
@@ -1,7 +1,16 @@
 public class REDUCEE {
-    public static void main(int args) {
-
-    } {
-
+    public static void main(int n) {
+        int steps = 0;
+        while (n > 0) {
+            boolean even_num = n % 2 == 0;
+            boolean odd_num = n % 2 == 1;
+            if (even_num) {
+                n = n / 2;
+            } else if (odd_num) {
+                n--;
+            }
+            steps++;
+        }
+        System.out.println(steps);
     }
 }
--- src/main.java
+++ src/main.java
@@ -1,5 +1,6 @@
 public class main {
     public static void main(String[] args) {
         FizzBuzz.doFizzBuzz();
+        REDUCEE.main(5);
     }
 }
